d843d96f59728745dbb4eb12284ec298121fbbf8
Ochoa <alan-daniel.ochoa@atos.net>
2021-12-21 17:17:22 -0600

    display data in a table

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,5 +1,5 @@
-
 import React, { useState, useEffect} from 'react'
+import * as ReactBootStrap from 'react-bootstrap'
 
 const url = 'https://restcountries.com/v3.1/all';
 
@@ -19,35 +19,60 @@ const Countries = () => {
         fetchCountries()
     }, [])
 
-    return <>
-        {countries.map((country, index) => {
+    return (
+        <div>
+            <ReactBootStrap.Table striped bordered hover>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Capital</th>
+                        <th>Region</th>
+                        <th>Languages</th>
+                        <th>Population</th>
+                        <th>Flags</th>
+                    </tr>
+                    </thead>
+                    <tbody>
 
-            const{ name, capital, region, languages, population, flags  } = country
-            const { common } = name
-            const { png } = flags
+                    {countries.map((country, index) => {
 
-            const newLeng = languages || {}
-            const languagesValues = Object.values(newLeng) || {};
-            console.log(languagesValues);
-            console.log(languagesValues.join(', '));
-            const languagesString = languagesValues.join(', ')
+                        const{ name, capital, region, languages, population, flags  } = country
+                        const { common } = name
+                        const { png } = flags
 
-            console.log("The current iteration is: " + index);
-            console.log("The current element is: " + common);
-            console.log("\n");
+                        const newLeng = languages || {}
+                        const languagesValues = Object.values(newLeng) || {};
+                        console.log(languagesValues);
+                        console.log(languagesValues.join(', '));
+                        const languagesString = languagesValues.join(', ')
+
+                        console.log("The current iteration is: " + index);
+                        console.log("The current element is: " + common);
+                        console.log("\n");
 
 
-            return <div key={index}>
-                <h4>{common}</h4>
-                <p>{capital}</p>
-                <p>{region}</p>
-                <p>{languagesString}</p>
-                <p>{population}</p>
-                <img src={png} alt={common} />
-            </div>
+                        return( 
+                           
+                                
+                        <tr key={index}>
+                        <td>{common}</td>
+                        <td>{capital}</td>
+                        <td>{region}</td>
+                        <td>{languagesString}</td>
+                        <td>{population}</td>
+                        <td><img style={{height: '24px'}}  src={png} alt={common} /></td>
+                        </tr>
 
-        })}
-    </>
+                            
+                        )
+
+                    })}
+
+                    </tbody>
+            </ReactBootStrap.Table>
+
+    </div>
+    )
 
     
 }
