f26f1311f23b359060a6d39802cca1705671c24f
Ochoa <alan-daniel.ochoa@atos.net>
2021-12-22 01:02:29 -0600

    create Filter component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Countries from "./components/Countries";
+import Filter from "./components/Filter";
 
 
 function App() {
   return <>
+    <Filter/>
     <Countries/>
     </>
 }
